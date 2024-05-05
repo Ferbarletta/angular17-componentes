e1dad94e1d414d032b26f0998329bfa64d9c6a32
null <null>
2024-05-05 15:59:26 -0300

    Ejercicio1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { IniciarSesionComponent } from './pages/auth/iniciar-sesion/iniciar-sesion.component';
+import { RegistroComponent } from './pages/auth/registro/registro.component';
+import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: "home", component:HomeComponent},
+    {path:"panel-control", component:DashboardComponent}, 
+    {path:"iniciar-sesion", component:IniciarSesionComponent},
+    {path:"registro", component:RegistroComponent},
+    {path:"quienes-somos", component:QuienesSomosComponent},
+    {path:"servicios", component:ServiciosComponent}
+
+];
